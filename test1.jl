3fd67971120b8117a0db0069784769db0b2cd008
Mandar Chitre <mandar@nus.edu.sg>
2026-05-20 03:07:47 +0800

    test: commit temporary test code

--- test1.jl
+++ test1.jl
@@ -1,13 +1,13 @@
 using UnderwaterAcoustics
 using DataDrivenAcoustics
 
-env = UnderwaterEnvironment(seabed=Rock)
+env = UnderwaterEnvironment(seabed=Rock, bathymetry=200.0)
 pm = PekerisRayTracer(env; max_bounces=3)
 
 txpos = [0.0, -11.0]
-f = 1000.0
+f = 250.0
 tx = AcousticSource(txpos[1], txpos[2], f)
-rxpos = rand(2, 500) .* [100.0, -20.0] .+ [500.0, 0.0]
+rxpos = rand(2, 1000) .* [200.0, 30.0] .+ [5500.0, -100.0]
 tloss = Array{Float32}(undef, size(rxpos)[2])
 
 for i in 1 : 1 : size(rxpos)[2]
@@ -16,35 +16,52 @@ end
 
 using Plots
 
-rx = AcousticReceiverGrid2D(range(500.0, step=0.1, length=1000), range(-20.0, step=0.1, length=200))
+rx = AcousticReceiverGrid2D(range(5000.0, length=1000), range(-200.0, length=200))
 let x = transmission_loss(pm, tx, rx)
-   plot(env; xlims=(500,600), ylims=(-20,0))
+   plot(env; xlims=(5000,6000), ylims=(-200,0))
    plot!(rx, x; clim = (0,80))
    scatter!(rxpos[1,:], rxpos[2,:]; markersize = 1.5, markercolor =:green, markerstrokewidth = 0)
 end
 
 pm2 = DataDrivenPropagationModel(RBNN_2D(7))
 
-# loss = TransmissionLossMSE(pm, tx, [AcousticReceiver(rxpos[1,i], rxpos[2,i]) for i in 1:size(rxpos,2)], tloss) # .- (mean(tloss) + 10*log10(pm.model.nrays)))
-# DataDrivenAcoustics.fit!(pm, loss, ADTypes.AutoReverseDiff(compile=true); maxiters=10000, optimizer=BFGS(), callback=(i, l) -> begin
-#    @info l
-#    false
-# end)
+rx1 = AcousticReceiver(5500, -100)
+a = arrivals(pm, tx, rx1)
 
-a = arrivals(pm, tx, rx[91400])
-
-pm2 = let ps = pm2.params, c = pm.c, ω = 2π * tx.frequency, xz = location(rx[91400])
+pm2 = let ps = pm2.params, c = pm.c, ω = 2π * tx.frequency, xz = location(rx1)
    ps.A = map(x -> abs(x.phasor), a)
-   ps.θ = map(x -> x.arrival_angle, a) / 2π
-   ps.ϕ = map(x -> angle(x.phasor) + ω * (x.t - (xz[1] * cos(x.arrival_angle) + xz[3] * sin(x.arrival_angle)) / c), a) / 2π
+   ps.θ = map(x -> -x.arrival_angle, a) / 2π
+   ps.ϕ = map(x -> angle(x.phasor) + ω * (x.t - (xz[1] * cos(x.arrival_angle) - xz[3] * sin(x.arrival_angle)) / c), a) / 2π
    pm2(ps)
 end
 
-acoustic_field(pm, tx, rx[91400])
-acoustic_field(pm2(ps2), tx, rx[91400])
+acoustic_field(pm, tx, rx1)
+acoustic_field(pm2, tx, rx1)
 
 let x = transmission_loss(pm2, tx, rx)
-   plot(env; xlims=(500,600), ylims=(-20,0))
+   plot(env; xlims=(5000,6000), ylims=(-200,0))
    plot!(rx, x; clim = (0,80))
-   scatter!(rx[91400])
+   scatter!(rx1)
+end
+
+pm3 = DataDrivenPropagationModel(RBNN_2D(60))
+pm3.params.A .*= 1f-4
+# .- (mean(tloss) + 10*log10(pm3.model.nrays))
+
+rxs = [AcousticReceiver(rxpos[1,i], rxpos[2,i]) for i in 1:size(rxpos,2)]
+loss = TransmissionLossMSE(pm3, tx, rxs, tloss)
+opt = Adam(1e-5)
+DataDrivenAcoustics.fit!(pm3, loss; maxiters=5000, optimizer=opt, callback=(i, l) -> begin
+   @info l
+   l < 100
+   #false
+end)
+DataDrivenAcoustics.fit!(pm3, loss; maxiters=100, optimizer=BFGS(), callback=(i, l) -> begin
+   @info l
+   false
+end)
+
+let x = transmission_loss(pm3, tx, rx)
+   plot(env; xlims=(5000,6000), ylims=(-200,0))
+   plot!(rx, x; clim = (0,80))
 end
