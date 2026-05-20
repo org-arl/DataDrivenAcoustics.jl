5ce4d7749a954e58a05f1920e1fc3f41da3b17bb
Mandar Chitre <mandar@nus.edu.sg>
2026-05-21 01:33:29 +0800

    refactor: improve implementation, update documentation, add test cases

--- src/RBNN.jl
+++ src/RBNN.jl
@@ -1,27 +1,30 @@
 import Lux: LuxCore
 import Random: AbstractRNG
 
-export RBNN_2D
+export RayBasisNN_2D, RBNN_2D
 
 """
-    RBNN_2D(nrays)
+    RayBasisNN_2D(nrays)
 
 Ray basis neural network (RBNN) Lux layer for 2D acoustic propagation.
 `nrays` is the number of basis rays.
 """
-struct RBNN_2D <: LuxCore.AbstractLuxLayer
+struct RayBasisNN_2D <: LuxCore.AbstractLuxLayer
   nrays::Int
 end
 
-LuxCore.initialparameters(rng::AbstractRNG, l::RBNN_2D) = (
+# alias for backward compatibility with pre-release versions
+const RBNN_2D = RayBasisNN_2D
+
+LuxCore.initialparameters(rng::AbstractRNG, l::RayBasisNN_2D) = (
   A = 1e-4 * randn(rng, Float32, l.nrays),  # amplitudes
   ϕ = rand(rng, Float32, l.nrays),          # phases (scaled by 1/2π)
   θ = rand(rng, Float32, l.nrays)           # ray angles (scaled by 1/2π)
 )
 
-LuxCore.initialstates(::AbstractRNG, ::RBNN_2D) = NamedTuple()
+LuxCore.initialstates(::AbstractRNG, ::RayBasisNN_2D) = NamedTuple()
 
-function (l::RBNN_2D)(inp::AbstractMatrix, ps, st::NamedTuple)
+function (l::RayBasisNN_2D)(inp::AbstractMatrix, ps, st::NamedTuple)
   xz = @view inp[1:2, :]
   k = @view inp[3:3, :]
   k̂ = hcat(cospi.(2ps.θ), sinpi.(2ps.θ))
